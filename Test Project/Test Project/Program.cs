b41535795b1cde6bcd99403210331bc194309671
brianwhelan <kardkontrol@gmail.com>
2014-02-14 14:14:19 +0000

    Changed console.writeline
    did some stuff and it worked.

--- Test Project/Test Project/Program.cs
+++ Test Project/Test Project/Program.cs
@@ -10,7 +10,7 @@ namespace Test_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Im Brian");
             Console.ReadLine();
         }
     }
